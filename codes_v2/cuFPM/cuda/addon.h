109ad89740ff4bfd8793e654deac519219951d09
Hololab <hololab@mail.tsinghua.edu.cn>
2025-05-07 17:36:59 +0800

    Update addon.h
    Add CHECK_THROW function for checking if input is not gpuArray.

--- codes_v2/cuFPM/cuda/addon.h
+++ codes_v2/cuFPM/cuda/addon.h
@@ -1,6 +1,15 @@
 #include <cuda_runtime.h>
 #include <cufft.h>
+#include <string>
+#include <stdexcept>
+#include <vector>
 
 #define BLOCK_X 16
 #define BLOCK_Y 16
 #define BLOCK_SIZE (BLOCK_X * BLOCK_Y)
+
+#define STRINGIFY(x) #x
+#define STR(x) STRINGIFY(x)
+#define FILE_LINE __FILE__ ":" STR(__LINE__)
+#define CHECK_THROW(x) \
+	do { if (!(x)) throw std::runtime_error(std::string(FILE_LINE " check failed " #x)); } while(0)
